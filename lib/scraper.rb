0467d53e97e84e7b0c44ad10e4e8dea599e953c7
Isaiah Montoya <idmontoya@yahoo.com>
2021-04-04 00:02:24 -0800

    commit tree ea13dcc31b9b9d7e639519cc886318d9923e1a70

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css(".tout__label.heading.heading--level-4")
+
